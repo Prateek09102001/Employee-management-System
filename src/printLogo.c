1f776b7528683ad01a286eac26f1f4bb9af7db05
Prateek khandelwal <127758351+Prateek09102001@users.noreply.github.com>
2024-07-02 22:20:13 +0530

    Update printLogo.c

--- src/printLogo.c
+++ src/printLogo.c
@@ -5,7 +5,7 @@ void printLogo(void)
     system("cls");
     printf("\n\t\t\t   EMPLOYEE MANAGEMENT SYSTEM\n");
     printf("\n\t\t----------------------------------------------------\n");
-    printf("\t\t\tPOORNIMA COLLEGE OF ENGINEERING\n");
-    printf("\t\t\t\tSitapura, Jaipur\n");
+    printf("\t\t\tHello Coder Software Privite Limited\n");
+    printf("\t\t\t\tMansarover, Jaipur\n");
     printf("\t\t----------------------------------------------------\n\n");
 }
